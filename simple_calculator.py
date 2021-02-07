3228f80d102737c6b078b5762cb2246d81615a86
briancatraguna <briancatraguna@rocketmail.com>
2021-02-07 23:54:35 +0700

    Added buttons list

--- simple_calculator.py
+++ simple_calculator.py
@@ -6,7 +6,9 @@ mainWindow.title("My Simple Calculator")
 mainWindow.geometry("1000x700+50+50")
 
 displayFrame = Frame(mainWindow,bg="gray",width=900,height=100)
-displayFrame.grid(row=0,column=0)
+displayFrame.grid(row=0,column=0,sticky="n")
 displayFrame.config(relief="sunken",borderwidth=5)
+
+buttons = ["1,2,3,4,5,6,7,8,9"]
 
 mainWindow.mainloop()
