0bca463a91c34efbb7538b3a6301e79cd4efc191
briancatraguna <briancatraguna@rocketmail.com>
2021-02-07 23:47:00 +0700

    test

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,8 +1,11 @@
-import tkinter
+from tkinter import *
 
-mainWindow = tkinter.Tk()
+mainWindow = Tk()
 
 mainWindow.title("My Simple Calculator")
 mainWindow.geometry("1000x700+50+50")
+
+displayFrame = Label(mainWindow,bg="skyblue",text="test")
+displayFrame.grid(row=0,column=0)
 
 mainWindow.mainloop()
